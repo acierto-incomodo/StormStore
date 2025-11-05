46112b10198ef8cfcc50d4be7669cbe6ecf20d79
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2025-11-05 21:07:15 +0100

    Publicar CardinalAI MultiModel v1.128.1450

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
-    sendMessage: (channel, data) => ipcRenderer.send(channel, data),
-    onMessage: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(...args))
-});
