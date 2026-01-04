662e56f77b706a2ab1a596073971a7c421489be6
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-01-04 14:47:21 +0100

    Update repository

--- debian-machine-install-auto.sh
+++ debian-machine-install-auto.sh
@@ -36,7 +36,7 @@ cat << "EOF"
  | || | | \__ \ || (_| | | |  __/ |                                       
 |___|_| |_|___/\__\__,_|_|_|\___|_|                                       
 EOF
-printf "${C_RESET}By StormGamesStudios\n\n"
+printf "${C_RESET}By StormGamesStudios (v1.0.0)\n\n"
 print_info "Iniciando el script de configuración automática para Debian Trixie."
 print_info "Este script se ejecutará como root y configurará todo el entorno."
 sleep 3
